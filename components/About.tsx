df8063b3c8862c7cc13d540b04ba16b88a8e4639
HRC4S <gedoldown@gmail.com>
2026-07-23 02:40:09 +0700

    Changeser

--- components/About.tsx
+++ components/About.tsx
@@ -4,9 +4,9 @@ import { TextAnimate } from "@/components/ui/text-animate"
 export default function AboutMe() {
   return (
     <section className="relative h-screen mt-20 flex flex-col items-end justify-start text-center overflow-x-hidden px-6 sm:px-12 lg:px-15">
-      <h1 className="self-start max-w-full text-3xl sm:text-4xl md:text-5xl font-bold text-white leading-tight">
+      <h1 className="self-start max-w-full text-7xl font-bold text-white leading-tight font-display">
         <TextAnimate animation="slideUp" by="word">
-          Hello, I'm Dega Anjam Piani
+          Hello World.
         </TextAnimate>
       </h1>
       <div className="self-start mt-4 max-w-2xl text-base sm:text-lg text-white text-left">
